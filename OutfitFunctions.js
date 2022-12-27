f91a1390742010518fc3a7f5be0327f6c4417dea
morganparslow <morgan.parslow@gmail.com>
2022-12-26 19:51:16 -0700

    hopefully centered

---
+++ OutfitFunctions.js
@@ -0,0 +1,16 @@
+
+//asking for user input
+var P = window.prompt("What piece of clothing?");
+var C = window.prompt("What color is the piece?");
+var D = window.prompt("What is one detail about the piece?");
+
+function Clothing(P, C, D) {
+    this.piece = P;
+    this.color = C;
+    this.Detail = D;
+}
+
+
+
+ 
+
